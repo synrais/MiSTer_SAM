f5ed333bad95ad87f5f79fa38af1eeb2a77e5b24
synrais <78070154+synrais@users.noreply.github.com>
2025-08-26 01:31:05 +1000

    Update MiSTer_SAM_on.sh

--- MiSTer_SAM_on.sh
+++ MiSTer_SAM_on.sh
@@ -195,6 +195,7 @@ function init_vars() {
     declare -g scaler_info_pid=""
     declare -g scaler_info_fd=""
     declare -g scaler_info_path="${mrsampath}/scaler_info"
+    declare -g scaler_info_last=""
     declare -gi scaler_ignore_until=0
     declare -g repository_url="https://github.com/mrchrisster/MiSTer_SAM"
     declare -g branch="main"
@@ -1049,13 +1050,14 @@ function run_countdown_timer() {
         ((counter--))
 
         if [[ "$skip_black_screens" == "yes" || "$skip_static_screens" == "yes" ]]; then
-            local sinfo=""
-            local line=""
             if [[ -n "$scaler_info_fd" ]]; then
-                while read -t 0 -u "$scaler_info_fd" line; do
-                    sinfo="$line"
+                local chunk
+                while IFS= read -r -t 0 -u "$scaler_info_fd" chunk; do
+                    scaler_info_last+="$chunk"
+                    scaler_info_last="${scaler_info_last##*$'\r'}"
                 done
             fi
+	        local sinfo="$scaler_info_last"
             if (( SECONDS >= scaler_ignore_until )); then
                 local stime=$(echo "$sinfo" | grep -o 'StaticTime=[0-9.]*' | cut -d= -f2)
                 [[ -z "$stime" ]] && stime=0
